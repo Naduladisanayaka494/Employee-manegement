db3b671e84d2e574e77e9128ed61a57837344f34
Naduladisanayaka494 <disanayakanadula494@gmail.com>
2025-03-06 16:08:25 +0530

    feat: Implemented MongoDB connection and JWT authentication
    - Configured MongoDB connection using Spring Data MongoDB
    - Set up JWT authentication for secure user access
    - Added necessary dependencies for MongoDB and JWT
    - Created security configurations for authentication and authorization

---
+++ src/main/java/com/employeemanegement/employee_manegement/EmployeeManegementApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanegement.employee_manegement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManegementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManegementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeemanegement/employee_manegement/EmployeeManegementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanegement.employee_manegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManegementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
